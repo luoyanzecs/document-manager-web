284aaed96db102c8b12554bb3d74fe438477cf1f
luoyanze <luoyanzeze@icloud.com>
2022-02-21 23:25:37 +0800

    feat(lyz): 添加avatar组件

--- src/components/ZAvatar.vue
+++ src/components/ZAvatar.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="w-14 h-14 bg-gray-500">
-    <slot></slot>
+  <div class="w-12 h-12 rounded-full p-1 bg-gray-300 pos-center">
+    <component :is="iconComponent" class="w-8 h-8 text-white"/>
   </div>
 </template>
 
-<script setup>
+<script setup >
+import { defineProps, computed, } from "vue";
+
+const props = defineProps({
+  icon: {}
+});
+
+const iconComponent = computed(() => props.icon)
+
 </script>
 
 <style scoped>

b6b0f37715d82381d1ff9b91d0d390f820e6c44a
luoyanze <luoyanzeze@icloud.com>
2022-02-23 00:12:23 +0800

    feat(lyz): 添加switch组件

--- src/Test.vue
+++ src/Test.vue
@@ -1,84 +1,12 @@
 <template>
-  <z-tree :catalogue="items"/>
+  <z-switch left="管理员" right="员工" v-model:value="value"/>
 </template>
 
 <script setup>
-import ZTree from "@/components/common/ZTree";
+import ZSwitch from "@/components/common/ZSwitch";
+import {ref} from "vue";
 
-const items = [
-  {
-    id: '0001',
-    title: 'Level one 1',
-    children: [
-      {
-        id: '0002',
-        title: 'Level two 1-1',
-        children: [
-          {
-            title: 'Level three 1-1-1',
-            children: []
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '0003',
-    title: 'Level one 2',
-    children: [
-      {
-        id: '0004',
-        title: 'Level two 2-1',
-        children: [
-          {
-            id: '0005',
-            title: 'Level three 2-1-1',
-            children: []
-          },
-        ],
-      },
-      {
-        id: '0006',
-        title: 'Level two 2-2',
-        children: [
-          {
-            id: '0007',
-            title: 'Level three 2-2-1',
-            children: []
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '0008',
-    title: 'Level one 3',
-    children: [
-      {
-        id: '0009',
-        title: 'Level two 3-1',
-        children: [
-          {
-            id: '0010',
-            title: 'Level three 3-1-1',
-            children: []
-          },
-        ],
-      },
-      {
-        id: '0011',
-        title: 'Level two 3-2',
-        children: [
-          {
-            id: '0012',
-            title: 'Level three 3-2-1',
-            children: []
-          },
-        ],
-      },
-    ],
-  }
-]
+const value = ref(false)
 </script>
 
 <style scoped>

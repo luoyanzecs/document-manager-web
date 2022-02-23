55fe66da809101d3a9301a3578fc964413153c1d
luoyanze <luoyanzeze@icloud.com>
2022-02-24 02:17:11 +0800

    feat(lyz): 添加评论区组件

--- src/components/common/ZButton.vue
+++ src/components/common/ZButton.vue
@@ -1,10 +1,19 @@
 <template>
   <button class="min-w-4 bg-blue-500 rounded-xl shadow py-2 px-4 text-sm h-9 text-white subpixel-antialiased tracking-widest font-light flex justify-center items-center">
+    {{ fill }}
     <slot></slot>
   </button>
 </template>
 
 <script setup>
+import {defineProps} from "vue";
+
+defineProps({
+  fill: {
+    type:String,
+    required: false
+  }
+})
 </script>
 
 <style scoped>

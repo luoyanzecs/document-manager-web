5d8f0e46ff7002be7cd621e18ce3427199429c71
luoyanze <luoyanzeze@icloud.com>
2022-02-24 21:28:44 +0800

    feat(lyz): 评论区优化

--- src/components/common/head/ZCollapse.vue
+++ src/components/common/head/ZCollapse.vue
@@ -1,7 +1,7 @@
 <template>
   <z-header-button v-show="!show" @click="switchTool">工具</z-header-button>
   <div v-show="show"
-      class="flex flex-col gap-6 pt-4 px-3 absolute bg-gray-100 absolute right-0 top-0 h-screen z-10">
+      class="flex flex-col gap-6 pt-4 px-3 absolute bg-gray-100 right-0 top-0 h-screen z-10">
     <z-header-button @click="switchTool">收起</z-header-button>
     <slot name="collapse"></slot>
   </div>
@@ -13,9 +13,7 @@ import ZHeaderButton from "@/components/common/ZButton";
 
 const show = ref(false)
 
-const switchTool = () => {
-  show.value = !show.value
-}
+const switchTool = () => show.value = !show.value
 
 </script>
 <style>
--- src/views/ZUserView.vue
+++ src/views/ZUserView.vue
@@ -29,48 +29,27 @@
         </Header>
       <div class >
         <Editor ref="editor"/>
-        <!--          TODO: 评论区， 显示时间， 内容， user 注意字体颜色和对齐方式-->
-        <el-divider content-position="left">
-          <el-icon color="#909399" :size="30"><comment/></el-icon>
-        </el-divider>
-        <ZComment></ZComment>
+        <hr class="my-4">
+        <z-comment :info="myInfo" :comments-list="comments"/>
       </div>
     </div>
 </template>
 
 
 <style>
-.el-tree-node:focus>.el-tree-node__content{
-      background-color: #5daaf0;
-}
-.el-tree-node__content:hover
- { 
-        background:#89c2f7;
-}
-.el-tree-node__label {
-width: auto;
-margin-top: 12px;
-float: right;
-font-family: “Trebuchet MS”, Arial, Helvetica, sans-serif;
-font-size: 14px;
-color: #999999;
-line-height: 25px;
-letter-spacing: 1px;
-}
 </style>
 
 <script setup>
 import Editor from "@/components/ZEditor";
-import ZComment from "@/components/ZComment"
-import { ElDivider } from 'element-plus'
 import { ref, reactive } from 'vue'
-import { Comment, UserFilled } from "@element-plus/icons-vue"
+import { UserFilled } from "@element-plus/icons-vue"
 import Header from "@/components/common/ZHeader";
 import HeadTool from "@/components/user/HeadTool";
 import ZAside from "@/components/common/ZAside";
 import ZButton from "@/components/common/ZButton";
 import ZAvatar from "@/components/ZAvatar";
 import ZTree from "@/components/common/ZTree";
+import ZComment from "@/components/common/ZComment";
 
 const editor = ref()
 
@@ -102,62 +81,7 @@ const items = [
         children: [
           {
             title: 'Level three 1-1-1',
-            children: [
-              {
-                id: '0002',
-                title: 'Level two 1-1',
-                children: [
-                  {
-                    title: 'Level three 1-1-1',
-                    children: [
-                      {
-                        id: '0002',
-                        title: 'Level two 1-1',
-                        children: [
-                          {
-                            title: 'Level three 1-1-1',
-                            children: [
-                              {
-                                id: '0002',
-                                title: 'Level two 1-1',
-                                children: [
-                                  {
-                                    title: 'Level three 1-1-1',
-                                    children: [
-                                      {
-                                        id: '12313',
-                                        title: 'Level two 1-1',
-                                        children: [
-                                          {
-                                            title: 'Level three 1-1-1',
-                                            children: [
-                                              {
-                                                id: '0002',
-                                                title: 'Level two 1-1',
-                                                children: [
-                                                  {
-                                                    title: 'Level three 1-1-1',
-                                                    children: []
-                                                  },
-                                                ],
-                                              },
-                                            ],
-                                          },
-                                        ],
-                                      },
-                                    ],
-                                  },
-                                ],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
+            children: []
           },
         ],
       },
@@ -219,6 +143,67 @@ const items = [
       },
     ],
   }
+]
+
+const myInfo = ref({
+  name: 'Lana Del Rey',
+  header: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+  id: 19870621,
+})
+const comments = [
+  {
+    commentId: '0001',
+    name: 'Lana Del Rey',
+    id: 19870621,
+    headImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+    comment: '我发布一张新专辑Norman Fucking Rockwell,大家快来听啊',
+    time: '2019年9月16日 18:43',
+    reply: [
+      {
+        commentId: '0002',
+        name: 'Taylor Swift',
+        id: 19891221,
+        headImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+        comment: '我很喜欢你的新专辑！！',
+        time: '2019年9月16日 18:43',
+      },
+      {
+        commentId: '0003',
+        name: 'Ariana Grande',
+        id: 1123,
+        headImg: 'https://ae01.alicdn.com/kf/Hf6c0b4a7428b4edf866a9fbab75568e6U.jpg',
+        comment: '别忘记宣传我们的合作单曲啊',
+        time: '2019年9月16日 18:43',
+      }
+    ]
+  },
+  {
+    commentId: '0004',
+    name: 'Taylor Swift',
+    id: 19891221,
+    headImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+    comment: '我发行了我的新专辑Lover',
+    time: '2019年9月16日 18:43',
+    reply: [
+      {
+        commentId: '0005',
+        name: 'Lana Del Rey',
+        id: 19870621,
+        HeadImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+        comment: '新专辑和speak now 一样棒！',
+        time: '2019年9月16日 18:43',
+      }
+    ]
+  },
+  {
+    commentId: '0006',
+    name: 'Norman Fucking Rockwell',
+    id: 20190830,
+    headImg: 'https://ae01.alicdn.com/kf/Hdd856ae4c81545d2b51fa0c209f7aa28Z.jpg',
+    comment: 'Plz buy Norman Fucking Rockwell on everywhere',
+    time: '2019年9月16日 18:43',
+    reply: []
+  },
 ]
 
 </script>

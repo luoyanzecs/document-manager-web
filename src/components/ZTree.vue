46faf7e6fad5e0d961ea0072602bcaf4bebdb000
luoyanze <luoyanzeze@icloud.com>
2022-04-10 20:17:20 +0800

    树形菜单优化

--- src/components/ZTree.vue
+++ src/components/ZTree.vue
@@ -1,28 +1,18 @@
-<template>
-  <svg v-if="level === 0 && isMenuLoad"
-       class="text-gray-500 w-8 h-8 animate-spin mx-auto mt-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-  </svg>
-  <template v-if="catalogue.length !== 0">
-    <ul v-for="(item, index) in catalogue" :key="item.id" :class="[{'border-l' : level !== 0}, 'border-gray-300', 'ml-1', 'pl-1']">
-      <li v-if="!item.isDir" :ref="treeItemRef" @click.stop="fileClickHandler(item)">
-        <span :class="['cata-link', {'bg-blue-200': item.id === chooseId}]">{{ item.title }}</span>
-      </li>
-      <li v-else :ref="treeItemRef">
-        <span class="catalogue-base text-gray-500" @click.stop="folderClickHandler(index)">{{ item.title }}</span>
-        <transition name="scrollin">
-          <div v-show="isShow[index]" style="height: 0; transition: all .3s ease-in-out;">
-            <z-tree :catalogue="item.children" :level="level + 1" :choose-id="chooseId" :is-show-componet="isShow[index]" @select-file="fileClickHandler"/>
-          </div>
-        </transition>
-      </li>
-    </ul>
-  </template>
+<template v-if="catalogue.length !== 0">
+  <ul class="overflow-x-visible select-none">
+    <li v-for="(item, index) in catalogue" :key="item.id" :ref="treeItemRef" :class="[{'border-l' : level !== 0}, 'border-gray-300', 'ml-1', 'pl-1']">
+      <div v-if="!item.isDir" @click.stop="fileClickHandler(item)" :class="['cata-link', {'bg-blue-200': item.id === chooseId}]">{{ item.title }}</div>
+      <div v-else @click.stop="folderClickHandler(index)" class="catalogue-base text-gray-500">{{ item.title }}</div>
+      <transition name="scrollin" v-if="item.isDir">
+        <z-tree v-show="isShow[index]" @select-file="fileClickHandler"
+                :catalogue="item.children" :level="level + 1" :choose-id="chooseId" :is-show-componet="isShow[index]" />
+      </transition>
+    </li>
+  </ul>
 </template>
 
 <script setup>
-import {defineProps, defineEmits, onMounted, ref, watch, onBeforeUpdate} from "vue";
+import {defineProps, defineEmits, ref, watch, onBeforeUpdate} from "vue";
 import {sleep} from "@/tool/utils";
 
 const props = defineProps({
@@ -30,20 +20,14 @@ const props = defineProps({
   chooseId: {type: String, required: false},
   level: {type: Number, default: 0, required: false},
   isShowComponet: {type: Boolean, default: false, required: false},
-  isMenuLoad: {type: Boolean, default: false, required: false}
 })
 
-const emit = defineEmits(['selectFile', 'update:chooseId'])
+const emit = defineEmits(['selectFile'])
 const isShow = ref([])
 
 let treeItemRefs = []
-
 const treeItemRef = el => el && treeItemRefs.push(el)
-
 onBeforeUpdate(() => treeItemRefs = [])
-
-onMounted(() => {
-})
 
 watch(
     () => props.isShowComponet,
@@ -66,8 +50,8 @@ const folderClickHandler = (index) => {
         isShow.value[index] = false
       })
     } else {
-      subCtx.style = `height: 0; transition: all .2s ease-in-out;`
-      subCtx.style = `height: ${treeItemRefs[index].clientHeight * (subCtx.childElementCount)}px; transition: all .2s ease-in-out;`
+      subCtx.style = `height: 0; transition: all .2s ease-in-out overflow: hidden;`
+      subCtx.style = `height: ${treeItemRefs[index].clientHeight * (subCtx.childElementCount)}px; transition: all .2s ease-in-out; overflow: hidden;`
       isShow.value[index] = true
       sleep(300).then(() => subCtx.style = '')
     }
